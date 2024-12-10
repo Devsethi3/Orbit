4e888413b0eced843cc8c6b7f715a250dbc8e1ba
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-12-10 21:55:27 +0530

    Update Features.tsx

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -51,7 +51,6 @@ const Features = () => {
           </motion.p>
         </div>
 
-        {/* */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {featuresList.map((feature, index) => (
             <motion.div
