728f8d67e1ef042a25ac72e421513539d3d97aab
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-08 22:38:39 +0530

    Update useCanvasCursor.tsx

--- src/hooks/useCanvasCursor.tsx
+++ src/hooks/useCanvasCursor.tsx
@@ -255,7 +255,7 @@ const useCanvasCursor = (): void => {
 
 export default useCanvasCursor;
 
-// Extend CanvasRenderingContext2D type to include custom properties
+// Extend CanvasRenderingContext2D type to include custom
 declare global {
   interface CanvasRenderingContext2D {
     running: boolean;
