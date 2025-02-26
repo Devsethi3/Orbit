65676548991ca1819623dd42b48c548ae1ad8e9b
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-02-26 23:11:46 +0530

    Update RemoveDialog.tsx

--- src/components/RemoveDialog.tsx
+++ src/components/RemoveDialog.tsx
@@ -46,7 +46,6 @@ const RemoveDialog = ({ documentId, children }: RemoveDialogProps) => {
         router.push("/dashboard");
       }, 100);
     } catch (error) {
-      //  
       if (error && typeof error === "object" && "message" in error) {
         const errorMessage = (error as { message: string }).message;
         if (errorMessage.includes("Document not found")) {
