a855d4b0c2c698c910be0587a77e5be822ed4973
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-03-02 23:00:37 +0530

    Update ShareDialog.tsx

--- src/components/ShareDialog.tsx
+++ src/components/ShareDialog.tsx
@@ -44,7 +44,7 @@ export const ShareDialog = ({
     localStorage.setItem("emailInput", emailInput);
   }, [emailInput]);
 
-  // Save public link to localStorage 
+  // Save public link to 
   useEffect(() => {
     if (publicLink) {
       localStorage.setItem(`publicLink-${documentId}`, publicLink);
