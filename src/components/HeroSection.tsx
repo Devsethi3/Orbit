d017b2138e4423b39b77347ad6d06610f38f0840
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-03-04 23:21:02 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -87,7 +87,7 @@ const HeroSection = () => {
               </Link>
             </motion.div>
 
-            {/* Demo Vid */}
+            {/* Demo V */}
             <motion.div
               className="w-full max-w-5xl mb-20"
               initial={{ opacity: 0, y: 20 }}
