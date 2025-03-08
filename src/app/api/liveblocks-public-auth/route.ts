61e03fbfc117d65615387be9c3239b6650c10835
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-03-08 23:27:24 +0530

    Update route.ts

--- src/app/api/liveblocks-public-auth/route.ts
+++ src/app/api/liveblocks-public-auth/route.ts
@@ -6,7 +6,7 @@ const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
-// Generate a consistent color based on user's name
+// Generate a consistent color based on user's
 function generateUserColor(name: string): string {
   const nameToNumber = name
     .split("")
