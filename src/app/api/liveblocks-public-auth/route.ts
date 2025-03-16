edb1c981f8f1b2d6c1615c3b84dbff4e29a77f03
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-03-16 22:47:19 +0530

    Update route.ts

--- src/app/api/liveblocks-public-auth/route.ts
+++ src/app/api/liveblocks-public-auth/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: NextRequest) {
       },
     });
 
-    // Add permission 
+    // Add
     session.allow(room, session.FULL_ACCESS);
 
     // Authorize the session
